13d0c74b405ff1db13ea4a3db40416794ddb319f
R.C. Long <deac.long@wsu.edu>
2018-03-27 06:34:35 -0700

    Install packages

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express()
 
 const PORT = 5000
 
+app.use(express.static('public'));
+
 app.set("port", process.env.PORT || PORT)
 
 var server = app.listen(app.get("port"), function () {
   console.log(`Server running at port ${server.address().port}...`)
 })
+
+var routes = require('./src/routes/fileAPI.routes.js')(app)
